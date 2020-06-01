f74e34b6caef10fa4170409e9e43a009e07da1a2
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-31 18:09:36 -0700

    pointer

--- pointer-practice.c
+++ pointer-practice.c
@@ -3,7 +3,10 @@
 int main() {
   int i = 10;
   int j = 20;
+  //int* is the type of pointer and p is the variable to store
+  //address of i
   int* p = &i;
+  
   printf("Value of i=%d, address=%p\n", i, p);
   p = &j;
   printf("Value of i=%d, j=%d, address=%p, value of p=%d\n", i, j, p, *p);
