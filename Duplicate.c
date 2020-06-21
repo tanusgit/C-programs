34b6c0d1cb293554e785ac90974fe2322aa47d9f
tanu <24976766+tanusgit@users.noreply.github.com>
2020-06-21 12:26:29 -0700

    Duplicate.c

--- Duplicate.c
+++ Duplicate.c
@@ -3,10 +3,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+//int dup2(int oldfd, int newfd)
 int main()
 {
     int file_desc = open("out.txt", O_WRONLY | O_APPEND);
     // here the newfd is the file descriptor of stdout (i.e. 1)
+    //dup2 takes 2 arguments one integer value and 1
     dup2(file_desc, 1);
     // All the printf statements will be written in the file
     // "out.txt"
